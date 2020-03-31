fd14904ff28e870027b8168c066a40ba1e4c7dab
advaithasabnis <advait.iitb@gmail.com>
2020-03-31 02:46:37 -0400

    Added provincial data
    Added top bars to provinces
    Added Alberta, British Columbia and Ontario

--- js/collapsible.js
+++ js/collapsible.js
@@ -7,10 +7,10 @@ window.onload = function () {
 	  coll[i].addEventListener("click", function() {
 		this.classList.toggle("active");
 		var panel = this.parentElement.nextElementSibling;
-		if (panel.style.maxHeight) {
-		  panel.style.maxHeight = null;
+		if (panel.style.display === "block") {
+		  panel.style.display = "none";
 		} else {
-		  panel.style.maxHeight = 1000 + 'px';
+		  panel.style.display = "block";
 		}
 	  });
 	}
