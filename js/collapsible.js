f483d1836f1161af644705e2f51f7e98016bdddb
advaithasabnis <advait.iitb@gmail.com>
2020-04-11 13:49:42 -0400

    morning update

--- js/collapsible.js
+++ js/collapsible.js
@@ -14,4 +14,22 @@ window.addEventListener('DOMContentLoaded', function () {
 		}
 	  });
 	}
+	
+	var modal = document.getElementById("myModal");
+	var btn = document.getElementById("myBtn");
+	var span = document.getElementsByClassName("close")[0];
+
+	btn.onclick = function() {
+	  modal.style.display = "block";
+	}
+	
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+		modal.style.display = "none";
+	  }
+	}
 })
