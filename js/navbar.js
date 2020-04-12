c5222870b124275915d80d423229194dab7bb96e
advaithasabnis <advait.iitb@gmail.com>
2020-04-11 20:12:23 -0400

    evening update

--- js/navbar.js
+++ js/navbar.js
@@ -6,3 +6,27 @@ function navbar() {
     x.className = "topnav";
   }
 }
+
+function bycases() {
+  var x = document.getElementById("byCases");
+  var y = document.getElementById("byDeaths");
+  x.style.maxHeight = 2000 + "px";
+  y.style.maxHeight = 0;
+  
+  var a = document.getElementById("btnCases");
+  var b = document.getElementById("btnDeaths");
+  a.style.backgroundColor = "white";
+  b.style.backgroundColor = "#dcdcdc";
+}
+
+function bydeaths() {
+  var x = document.getElementById("byCases");
+  var y = document.getElementById("byDeaths");
+  x.style.maxHeight = 0;
+  y.style.maxHeight = 2000 + "px";  
+  
+  var a = document.getElementById("btnCases");
+  var b = document.getElementById("btnDeaths");
+  a.style.backgroundColor = "#dcdcdc";
+  b.style.backgroundColor = "white";
+}
