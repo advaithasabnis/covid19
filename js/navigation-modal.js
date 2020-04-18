807e6b7e33213f8ddb0747c425f2cce0d18f5da7
advaithasabnis <advait.iitb@gmail.com>
2020-04-18 15:25:24 -0400

    moving to new domain

--- js/navigation-modal.js
+++ js/navigation-modal.js
@@ -22,12 +22,25 @@ window.addEventListener('DOMContentLoaded', function () {
 		menu.className = "topnav";
 	  }
 	}
+	
+	var transition = document.getElementById("transitionModal");
+	var span2 = document.getElementsByClassName("close2")[0];
+	window.onload = function () {
+		if (window.location.origin.includes('github')) {
+			transition.style.display = "block";
+		}
+	}
+	span2.onclick = function() {
+	  transition.style.display = "none";
+	}
 
 	window.onmousedown = function(event) {
 	  if (event.target == menu) {
 		menu.className = "topnav";
 	  } else if (event.target == modal) {
 		modal.style.display = "none";
+	  } else if (event.target == transition) {
+		transition.style.display = "none";
 	  }
 	}
 	
