e779432457243a25816fc57ff84d643f4200cef4
advaithasabnis <advait.iitb@gmail.com>
2020-04-23 13:34:30 -0400

    noon update

---
+++ js/slideshow.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', function () {
+	var slideIndex = 0;
+	carousel();
+	
+	function carousel() {
+		var i;
+		var x = document.getElementsByClassName("slide");
+		for (i = 0; i < x.length; i++) {
+			x[i].style.display = "none";
+		}
+		slideIndex++;
+		if (slideIndex > x.length) {slideIndex=1;}
+		x[slideIndex-1].style.display = "table-cell";
+		setTimeout(carousel, 5000);
+	}
+});
