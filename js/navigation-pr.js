a5399e93f843bcf4cd0610e1c0611c1070dc9816
advaithasabnis <advait.iitb@gmail.com>
2020-05-07 15:46:12 -0400

    speed optim

--- js/navigation-pr.js
+++ js/navigation-pr.js
@@ -39,12 +39,12 @@ function prSelector(item) {
 	var prmenu = document.getElementById("prselector");
 	prmenu.className = "selector-content";
 	
-	var coll = document.getElementsByClassName("panel");
+	var coll = document.getElementsByClassName("prpanel");
 	var tabBtns = document.getElementsByClassName("selector-item");
 	var i;
 	
 	for (i = 0; i < coll.length; i++) {
-		coll[i].style.maxHeight = 0 + "px";
+		coll[i].style.display = "none";
 	}
 	
 	for (i = 0; i < tabBtns.length; i++) {
@@ -54,5 +54,10 @@ function prSelector(item) {
 	var pr = item.value;
 	panel = document.getElementById(pr);
 	item.className += " active";
-	panel.style.maxHeight = 2500 + "px";
+	panel.style.display = "block";
+	
+	graphDivs = panel.getElementsByClassName("plotly-graph-div");
+	for (i = 0; i < graphDivs.length; i++) {
+		Plotly.relayout(graphDivs[i], {autosize: true});
+	}
 }
